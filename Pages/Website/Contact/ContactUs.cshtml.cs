c0b13b7a1dc54550d7f14eeba3514caa2b915878
Ashraful Islam <reapon.idb@gmail.com>
2023-08-30 14:08:13 +0600

    first web app push

---
+++ Pages/Website/Contact/ContactUs.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace school_web_app.Pages.Website.Contact
+{
+    public class ContactUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
